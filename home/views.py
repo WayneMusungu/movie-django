f5c8bec029d0bbee4bc256b614d959dc719a8d82
WayneMusungu <waynemuyera17@gmail.com>
2022-06-25 16:54:31 +0300

    Create a functionality that will search by type either movie or tv show

--- home/views.py
+++ home/views.py
@@ -3,29 +3,34 @@ import requests
 from django.http import HttpResponse
 
 # Create your views here.
-TMDB_API_KEY = "05e5be7a518e07b0cdd93bf0e133083a"
+MOVIE_API_KEY = "05e5be7a518e07b0cdd93bf0e133083a"
 
 def search(request):
     
-    results = []
+    '''
+    Get the query from the search box
+    '''
     query = request.GET.get('q')
+    print(query)
+    
+    '''
+    If the query is not empty
+    '''
     if query:
-        print("1")
-        data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
-        # print(data.json())
         
-        # temp = []
-        # for m in data.json() ["results"]:
-        #     if len(temp) < 6 :
-                
-        #         temp.append({"poster_path":m["poster_path"],"name":m["name"],"origin_country":m["origin_country"],"original_name":m["original_name"],"overview":m["overview"],"popularity": m["popularity"]})
-        #     else:
-        #         results.append(temp)
-                
-        #     results.append(temp) if len(temp) > 0 else None
+        # data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=true&query={query}")
+        data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=true&query={query}")
+        '''
+        Looking for the response of the request we use (data.json()) or (data.text)
+        '''
+        print(data.json())
+        # print(data.text)
+            
             
     else:
         return HttpResponse("Please enter a search query")
+    
+    
     
     
     
@@ -34,3 +39,4 @@ def search(request):
 def index(request):
     return render (request, "index.html")
 
+# https://api.themoviedb.org/3/search/tv?api_key=03401687fc726546579b53d2f0d65ee7&language=en-US&page=1&include_adult=false&query=batman
