f6ae1493c2e95a7ebbd82b5d6ee3201c63508566
WayneMusungu <waynemuyera17@gmail.com>
2022-06-26 09:48:02 +0300

    add an if statement that will either show tv name or movie title

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("tv/<int:tv_id>/", views.view_tv_detail, name="tvdetail"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 MOVIE_API_KEY = "05e5be7a518e07b0cdd93bf0e133083a"
@@ -19,6 +19,8 @@ def search(request):
     if query:
         
         # data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=true&query={query}")
+        
+        
         data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=true&query={query}")
         '''
         Looking for the response of the request we use (data.json()) or (data.text)
@@ -34,9 +36,15 @@ def search(request):
     
     
     
-    return render(request, 'results.html',{"data":data.json()})
+    return render(request, 'results.html',{"data":data.json(),"type": request.GET.get("type")})
 
 def index(request):
     return render (request, "index.html")
 
 # https://api.themoviedb.org/3/search/tv?api_key=03401687fc726546579b53d2f0d65ee7&language=en-US&page=1&include_adult=false&query=batman
+
+
+def view_tv_detail(request, tv_id):
+    data = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}?api_key={MOVIE_API_KEY}&language=en-US")
+    
+    return render (request, 'tv_detail.html',{"data":data.json()})
