d063206bea7d64bcb0f58c7ea17fa7fd66a23480
WayneMusungu <waynemuyera17@gmail.com>
2022-07-19 16:02:03 +0300

    Add heroku database

--- home/views.py
+++ home/views.py
@@ -3,9 +3,10 @@ import requests
 from django.http import HttpResponse, JsonResponse
 from .models import Comment
 from django.contrib.auth.models import User
-import home.wayne as wayne
+
 
 # Create your views here.
+MOVIE_API_KEY = "05e5be7a518e07b0cdd93bf0e133083a"
 
 def search(request):
     
@@ -20,11 +21,10 @@ def search(request):
     '''
     if query:
         
-        # data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={keys.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
+        # data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
         
         
-        
-        data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={wayne.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
+        data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
         '''
         Looking for the response of the request we use (data.json()) or (data.text)
         '''
@@ -46,18 +46,18 @@ def index(request):
 
 
 def view_tv_detail(request, tv_id):
-    data = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+    data = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}?api_key={MOVIE_API_KEY}&language=en-US")
     
-    recommendations = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}/recommendations?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+    recommendations = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}/recommendations?api_key={MOVIE_API_KEY}&language=en-US")
     
     return render (request, 'tv_detail.html',{"data":data.json(), "recommendations":recommendations.json(), "type":"tv"})
 
 
 
 def view_movie_detail(request, movie_id):
-    data = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+    data = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={MOVIE_API_KEY}&language=en-US")
     
-    recommendations = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+    recommendations = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={MOVIE_API_KEY}&language=en-US")
     
     return render (request, 'movie_detail.html',{"data":data.json(), "recommendations":recommendations.json(),"type":"movie" })
 
@@ -67,7 +67,7 @@ def view_trendings_results(request):
     type = request.GET.get("media_type")
     time_window = request.GET.get("time_window")
 
-    trendings = requests.get(f"https://api.themoviedb.org/3/trending/{type}/{time_window}?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+    trendings = requests.get(f"https://api.themoviedb.org/3/trending/{type}/{time_window}?api_key={MOVIE_API_KEY}&language=en-US")
     return JsonResponse(trendings.json())
 
 
@@ -85,7 +85,7 @@ def comment_page(request,movie_id):
         return redirect(f"/movie/{movie_id}/comments/")
         
     else:
-        data = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={wayne.MOVIE_API_KEY}&language=en-US")
+        data = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={MOVIE_API_KEY}&language=en-US")
         
         title = data.json()["title"]
         
