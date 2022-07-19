f80debe13b048ccbbe31c9fb484eb33e30a86f9e
WayneMusungu <waynemuyera17@gmail.com>
2022-07-19 15:39:39 +0300

    Add heroku database

--- MovieApp/settings.py
+++ MovieApp/settings.py
@@ -32,7 +32,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 MODE=config("MODE", default="dev")
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'waynemovie.herokuapp.com']
 
 
 # Application definition
@@ -82,13 +82,31 @@ WSGI_APPLICATION = 'MovieApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '5432',
+       }
+
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
